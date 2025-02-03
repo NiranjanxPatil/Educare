9d182a0ad7a802eb4c61aefadc1ba0bd336c7d45
Niranjan <niranjan41210@gmail.com>
2025-02-03 23:43:08 +0530

    changes

--- generate_solution.py
+++ generate_solution.py
@@ -1,34 +1,39 @@
 from manim import *
 
-class SquareArea(Scene):
+class TwoPlusTwoPlusTen(Scene):
     def construct(self):
-        square = Square(side_length=2, color=BLUE)
-        square.set_fill(BLUE, opacity=0.5)
-        side_length = MathTex("2").next_to(square, UP, buff=0.2).scale(1.5).set_color(YELLOW)
-        side_length_label = Tex("cm").next_to(side_length, RIGHT, buff=0.1).set_color(YELLOW)
+        background = Rectangle(width=14, height=8, fill_color=DARK_GREY, fill_opacity=1).to_edge(LEFT)
+        self.add(background)
 
-        area_text = MathTex("Area = side \\times side").to_edge(UP).set_color(GREEN)
-        area_equation = MathTex("Area = 2 \\times 2").next_to(area_text, DOWN, buff=1).set_color(GREEN)
-        area_result = MathTex("Area = 4").next_to(area_equation, DOWN, buff=0.5).set_color(GREEN).scale(1.5)
-        area_unit = Tex("cm$^2$").next_to(area_result, RIGHT, buff=0.1).set_color(GREEN)
+        two_plus_two = MathTex("2 + 2").set_color(YELLOW).scale(2).shift(2*UP + 2*LEFT)
+        self.play(Write(two_plus_two))
 
-        self.play(Create(square))
-        self.play(Write(side_length), Write(side_length_label))
+        plus_ten = MathTex("+ 10").set_color(BLUE).scale(2).next_to(two_plus_two, RIGHT)
+        self.play(Write(plus_ten))
+
+        equals = MathTex("=").set_color(GREEN).scale(2).next_to(plus_ten, RIGHT)
+        self.play(Write(equals))
+
+        four = MathTex("4").set_color(YELLOW).scale(2).next_to(two_plus_two, RIGHT)
+        self.play(TransformMatchingTex(two_plus_two, four))
+        self.play(FadeOut(plus_ten))
+        self.play(four.animate.next_to(equals,LEFT))
+
+
+        four_plus_ten = MathTex("4 + 10").set_color(ORANGE).scale(2).next_to(equals,LEFT)
+        self.play(TransformMatchingTex(four, four_plus_ten[0]))
+        self.play(Write(four_plus_ten[1:]))
+
+        fourteen = MathTex("14").set_color(PINK).scale(3).next_to(four_plus_ten,RIGHT)
+        self.play(TransformMatchingTex(four_plus_ten, fourteen), run_time=1)
+
+
+        circle = Circle(radius=1, color=PURPLE, fill_opacity=0.5).move_to(fourteen)
+        self.play(Create(circle), run_time=0.5)
+        self.play(circle.animate.scale(2), run_time=0.5)
+        self.play(FadeOut(circle))
         self.wait(1)
 
-        self.play(Write(area_text))
-        self.wait(1)
-        self.play(Write(area_equation))
-        self.wait(1)
-        self.play(Transform(area_equation, area_result))
-        self.play(Write(area_unit))
-
-        self.play(square.animate.scale(1.2).set_color(RED), rate_func=wiggle)
-        self.wait(1)
-        self.play(square.animate.scale(1/1.2).set_color(BLUE), rate_func=smooth)
-        self.wait(1)
-
-        self.play(Indicate(area_result), Indicate(area_unit))
+        final_answer = Tex("Therefore, 2 + 2 + 10 = 14").set_color(WHITE).scale(1.5).to_edge(DOWN)
+        self.play(Write(final_answer))
         self.wait(2)
-
-        self.play(*[FadeOut(mob) for mob in self.mobjects])
